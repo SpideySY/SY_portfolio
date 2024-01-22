1fd79d77b6bbefec4b433f17c65a258ba6827f49
SpideySY <141653960+SpideySY@users.noreply.github.com>
2024-01-22 18:25:23 +0800

    Commit via overwrite

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,15 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
